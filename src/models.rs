7fe30a72fc6957594315161f38e4802bb0294803
Fernando Paredes <nano@fdp.io>
2018-03-08 19:30:41 -0500

    ✨ Expose subtitle

--- src/models.rs
+++ src/models.rs
@@ -1,14 +1,18 @@
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Gem {
-  pub name: String,
-  pub version: String,
-  pub info: String,
-  pub homepage_uri: String,
+  name: String,
+  version: String,
+  info: String,
+  homepage_uri: String,
 }
 
 impl Gem {
   pub fn title(&self) -> String {
     format!("{} ({})", self.name, self.version)
+  }
+
+  pub fn subtitle(&self) -> String {
+    format!("{}", self.info)
   }
 }
 
@@ -29,5 +33,11 @@ mod tests {
   fn test_title() {
     let gem = test_gem();
     assert_eq!(gem.title(), "cocoapods (1.4.0)")
+  }
+
+  #[test]
+  fn test_subtitle() {
+    let gem = test_gem();
+    assert_eq!(gem.subtitle(), "CocoaPods manages library dependencies for your Xcode project.\n\nYou specify the dependencies for your project in one easy text file")
   }
 }
