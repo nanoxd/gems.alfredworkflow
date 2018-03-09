9096641dd9523b0317a76edbea6e3ee0bf0c5ef6
Fernando Paredes <nano@fdp.io>
2018-03-08 19:25:05 -0500

    ✨ Add title to Gem

--- src/models.rs
+++ src/models.rs
@@ -5,3 +5,29 @@ pub struct Gem {
   pub info: String,
   pub homepage_uri: String,
 }
+
+impl Gem {
+  pub fn title(&self) -> String {
+    format!("{} ({})", self.name, self.version)
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  fn test_gem() -> Gem {
+    Gem {
+      name: "cocoapods".to_string(),
+      version: "1.4.0".to_string(),
+      info: "CocoaPods manages library dependencies for your Xcode project.\n\nYou specify the dependencies for your project in one easy text file".to_string(),
+      homepage_uri: "https://github.com/CocoaPods/CocoaPods".to_string()
+    }
+  }
+
+  #[test]
+  fn test_title() {
+    let gem = test_gem();
+    assert_eq!(gem.title(), "cocoapods (1.4.0)")
+  }
+}
